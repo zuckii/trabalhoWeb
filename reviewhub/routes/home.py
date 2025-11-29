4ce42608dfa223422550dcf7f0c5e0f7bc0e41c5
Zuchi <zuchii.victor@gmail.com>
2025-11-29 13:07:17 -0300

    alterei coisas bobas de rotas e do botao 'sair'

--- reviewhub/routes/home.py
+++ reviewhub/routes/home.py
@@ -5,7 +5,11 @@ home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
 def homepage():
-    
+    # If the user is not authenticated and guest mode is not enabled,
+    # redirect to the login screen. Otherwise render the movies index.
+    if not session.get('user_id') and not session.get('guest_mode'):
+        return redirect(url_for('auth.login'))
+
     movies = get_all_movies()
     return render_template("home/index.html", movies=movies)
 
